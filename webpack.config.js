77f901f3d78571b2cbf0edbf88239c5f8af6a55d
Airidas Amšiejus <airidasams@gmail.com>
2018-04-18 21:46:15 +0300

    working react hello world

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,10 @@ Encore
     .enableVersioning(Encore.isProduction())
 
     // uncomment to define the assets of the project
-    .addEntry('js/app', './assets/js/app.js')
+    .addEntry('js/app', [
+        './assets/js/app.js',
+        './assets/js/react.jsx'
+    ])
     .addStyleEntry('css/app', './assets/css/app.scss')
 
     // uncomment if you use Sass/SCSS files
