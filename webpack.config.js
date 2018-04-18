4e8d5d6e34455729d274886ae065c5bb7ae2b94a
Airidas Amšiejus <airidasams@gmail.com>
2018-04-18 21:27:52 +0300

    add React

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ Encore
 
     // uncomment if you use Sass/SCSS files
     .enableSassLoader()
-
+    .enableReactPreset()
     // uncomment for legacy applications that require $/jQuery as a global variable
     // .autoProvidejQuery()
 ;
