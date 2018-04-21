c659abbe82ece497d958c12a059df4f520907ae6
Airidas Amšiejus <airidasams@gmail.com>
2018-04-21 23:00:54 +0300

    Grades and index update

--- src/Controller/GradesController.php
+++ src/Controller/GradesController.php
@@ -2,21 +2,23 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class GradesController extends Controller
 {
     /**
-     * @Route("/grades/{team}/{mentor}/{student}", name="grades")
+     * @Route("/grades", name="grades")
      */
-    public function index($team, $mentor, $student)
+    public function index(Request $request)
     {
         return $this->render('grades/index.html.twig', [
             'controller_name' => 'GradesController',
-            'team'         => $team,
-            'mentor'       => $mentor,
-            'luckyStudent' => $student,
+            'akademija' => $request->get('utm_source'),
+            'team'         => $request->get('utm_content'),
+            'mentor'       => $request->get('utm_campaign'),
+            'luckyStudent' => $request->get('utm_term'),
         ]);
     }
 }
