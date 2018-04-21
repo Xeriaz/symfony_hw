a1d6126d2892eaff709039f5f24c361f6eda3342
Airidas Amšiejus <airidasams@gmail.com>
2018-04-21 15:23:03 +0300

    updates

--- src/Form/FOSRegistrationFormType.php
+++ src/Form/FOSRegistrationFormType.php
@@ -2,24 +2,31 @@
 
 namespace App\Form;
 
+use FOS\UserBundle\Form\Type\RegistrationFormType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class FOS_RegistrationForm extends AbstractType
+class FOSRegistrationFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('website', UrlType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefault([
-
+            'website' => '',
         ]);
+    }
+
+    public function getParent()
+    {
+        return RegistrationFormType::class;
     }
 }
 
